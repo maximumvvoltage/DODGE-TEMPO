984f715ef15c2a1a490dfc9b3619cbe558237250
maximumvvoltage <146975484+maximumvvoltage@users.noreply.github.com>
2025-03-30 04:21:15 +0100

    its 4 am

--- DODGE-TEMPO/Assets/SCRIPTS/SongPicker.cs
+++ DODGE-TEMPO/Assets/SCRIPTS/SongPicker.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class SongPicker : MonoBehaviour
 {
-    public Animator camAnim;
-
     // Start is called before the first frame update
     void Start()
     {
